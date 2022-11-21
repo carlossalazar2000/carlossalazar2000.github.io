917f18fe1ab3ebe03d635934c338e27dc038e76f
Carlos Salazar <csalazar2018@fau.edu>
2022-11-21 14:03:51 -0500

    Updates

---
+++ precache-manifest.5d0cb852b4018de795c48019442287ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42695ecfe5dad7ca96d7c53cc4899bf6",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/index.html"
+  },
+  {
+    "revision": "de184510b49c901d8ade",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/css/2.d8077e82.chunk.css"
+  },
+  {
+    "revision": "bd3e5c33a94158613dfb",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/css/main.936a4e4a.chunk.css"
+  },
+  {
+    "revision": "de184510b49c901d8ade",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/js/2.d3efe588.chunk.js"
+  },
+  {
+    "revision": "cc29f13b73e9b5feda1a9785ab30d18d",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/js/2.d3efe588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3e5c33a94158613dfb",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/js/main.88d6d0a7.chunk.js"
+  },
+  {
+    "revision": "c77d51780464091ded04",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/js/runtime-main.8659cf57.js"
+  },
+  {
+    "revision": "efad71eaa2190a0ea23c6b52cb5777a0",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/media/businessSearchApp.efad71ea.JPG"
+  },
+  {
+    "revision": "6e2d42b2bd21e0a1e46f96f2451d857e",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/media/divoc.6e2d42b2.JPG"
+  },
+  {
+    "revision": "6a591f5fbbfd55e70ad9e1e99e10d7ea",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/media/encryptionProject.6a591f5f.JPG"
+  },
+  {
+    "revision": "2523522f809a9407d0bf7e2f1b2e40b8",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/media/profile.2523522f.jpg"
+  },
+  {
+    "revision": "4b76d4537c516a3d05167052e2f85eac",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/media/rakApp.4b76d453.JPG"
+  },
+  {
+    "revision": "d8ac66c71dd06fb8fa8f00b7dde7f844",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/media/recipeMeetsYou.d8ac66c7.JPG"
+  },
+  {
+    "revision": "6367ea31cd64dc515cd9c19506d0b37d",
+    "url": "/carlossalazar2000/carlossalazar2000.github.io/static/media/venditShoppingApp.6367ea31.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82abcf709dabef93a6e6a1178515dc2c.js"
+  "/carlossalazar2000/carlossalazar2000.github.io/precache-manifest.5d0cb852b4018de795c48019442287ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/carlossalazar2000/carlossalazar2000.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
